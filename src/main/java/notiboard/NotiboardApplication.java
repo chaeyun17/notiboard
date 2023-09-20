d7727a30b3e29fafc0c52d4de653922e74b2f7d7
benji <benji@parti.coop>
2023-09-20 23:26:14 +0900

    chore: 프로젝트 생성 및 세팅

---
+++ src/main/java/notiboard/NotiboardApplication.java
@@ -0,0 +1,13 @@
+package notiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiboardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NotiboardApplication.class, args);
+  }
+
+}
---
+++ src/test/java/notiboard/NotiboardApplicationTests.java
@@ -0,0 +1,13 @@
+package notiboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotiboardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
